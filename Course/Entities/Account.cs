d43c4db994b28cbda44ebb9ed6fdb3f793a8ae2b
Alessandro Freire <alessmarf@gmail.com>
2021-05-23 22:16:17 -0300

    Add project files.

---
+++ Course/Entities/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Course.Entities.Exceptions
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithLimit = withLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void WithDraw(double amount)
+        {
+            if(Balance <= amount)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            else if (WithLimit >= amount)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+
+            Balance -= amount;
+        }
+
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Course.Entities.Exceptions;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account ac = new Account(number, holder, initialBalance, withDrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("New Balance: " + );
+               
+            }
+        }
+    }
+}
